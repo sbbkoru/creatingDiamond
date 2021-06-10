897d0bf2957bb25487cdd1c518306e4256bf253c
Berkay <74363545+sbbkoru@users.noreply.github.com>
2021-06-10 19:24:30 +0300

    Add files via upload

---
+++ scriptFIXED.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// DIAMOND
+
+const diamond = function (num) {
+  if (num <= 0 || num % 2 === 0) return null;
+  let str = "";
+  for (let i = 0; i < num; i++) {
+    let space = Math.abs((num - 2 * i - 1) / 2);
+    str += " ".repeat(space);
+    str += "*".repeat(num - 2 * space);
+    str += "\n";
+  }
+  return str;
+};
+
+console.log(diamond(11));
